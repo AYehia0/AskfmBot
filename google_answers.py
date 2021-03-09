e2d3ca0c940f68e2d5ced3b3115f3830add5a2f1
AYehia0 <ahmedyehia611@gmail.com>
2021-03-09 16:15:46 +0200

    error checking

--- google_answers.py
+++ google_answers.py
@@ -11,10 +11,18 @@ cse_key = os.environ['C_ID']
 def google_search(search_term, api_key, cse_id, **kwargs):
     service = build("customsearch", "v1", developerKey=api_key)
     res = service.cse().list(q=search_term, cx=cse_id, **kwargs).execute()
-    return res['items']
+    
+    # checking for valid query
+    if 'items' in res.keys():
+        return res['items']
+    return None
 
 
-def get_answer(question)
+def get_answer(question):
     result = google_search(question, api_key, cse_key)
 
-    return result[1]['snippet']
+    # cheking for invalid query 
+    if result is not None:
+        return result[0]['snippet']
+    else:
+        return "What do you mean, i am stupid"
